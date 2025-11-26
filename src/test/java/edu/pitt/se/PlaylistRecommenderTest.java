719f221caa0240fc8090376079dfb479c85a4e53
John Haut <jjh128@pitt.edu>
2025-11-26 14:47:52 -0500

    Made a second test to check build actions

--- src/test/java/edu/pitt/se/PlaylistRecommenderTest.java
+++ src/test/java/edu/pitt/se/PlaylistRecommenderTest.java
@@ -7,6 +7,15 @@ public class PlaylistRecommenderTest {
 
     @Test
     public void placeholder() {
+        // classifyTest1: null list
+        // classifyTest2: empty list
+        // classifyTest3: high bpm 140
+        // classifyTest4: medium bpm 120
+        // classifyTest5: low bpm 99
+
+        // 
+
+        assertFalse(false);
         assertTrue(true);
     }
 }
